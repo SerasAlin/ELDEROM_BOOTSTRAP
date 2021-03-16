c417b6bcd4523e3ee118e4cdd2692caa048da453
alin <alin.seras@collinsongroup.com>
2021-03-16 13:12:54 +0200

    mesaj

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -48,4 +48,35 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
+
+    $(document).ready(function(){
+
+    // Get current page URL
+    var url = window.location.href
+    // remove # from URL
+    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+    // remove parameters from URL
+    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+    // select file name
+    url = url.substr(url.lastIndexOf("/") + 1)
+    url = '/' + url;
+    // If file name not avilable
+    if(url == ''){
+    url = '/';
+    }
+    // Loop all menu items
+    $('.navbar-nav.ml-auto li').each(function(){
+     // select href
+     var href = $(this).find('a').attr('href');
+     console.log(href)
+     console.log(url)
+     // Check filename
+     if(url == href){
+      // Add active class
+      $(this).find('a').addClass('active');
+     }
+    });
+
+});
+  
 })(jQuery); // End of use strict
